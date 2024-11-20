b9299109f43427ab6bfbb5df4998bb0795d69d1e
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2024-11-20 20:04:10 -0300

    Exercicio 23 e 24 feitos

---
+++ 23_bottles_on_the_wall.py
@@ -0,0 +1,10 @@
+for i in range(10, 1, -1):
+    print(i, "bottles of beer on the wall,")
+    print(i, "bottles of beer,")
+    print("Take one down,")
+    print("Pass it around,")
+    if (i - 1) == 1:
+        print("1 bottle of beer on the wall,")
+        print("1 bottle of beer,")
+
+print('No more bottles of beer on the wall!')
---
+++ 24_every_15_minutes.py
@@ -0,0 +1,4 @@
+for tempo in ["am", "pm"]:
+    for horas in [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]:
+        for minutos in [0, 15, 30, 45]:
+            print(f"{horas} : {minutos} {tempo}")
